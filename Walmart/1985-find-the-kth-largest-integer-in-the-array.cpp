732f7d60e9378276042d5166040390267d27011b
Kushagra Rastogi <56469617+kushagrarastogi2014@users.noreply.github.com>
2022-02-02 13:14:38 +0530

    Create 1985-find-the-kth-largest-integer-in-the-array.cpp

---
+++ Walmart/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    static bool cmp(string a,string b)
+    {
+        if(a.length()==b.length())
+        {
+            for(int i=0;i<a.length();i++)
+            {
+                if(a[i]==b[i])
+                    continue;
+                return a[i]<b[i];
+            }
+        }
+        return a.length()<b.length();
+    }
+    string kthLargestNumber(vector<string>& nums, int k) {
+        sort(nums.begin(),nums.end(),cmp);
+        return nums[nums.size()-k];
+    }
+};
